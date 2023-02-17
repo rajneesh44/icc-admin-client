015915c0d4bbae1678cda32faf689effeadcca36
rajneesh44 <rajneeshsharma338@gmail.com>
2023-02-17 23:01:35 +0530

    adding admin login flow

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,33 @@
+import { useEffect } from "react";
+import "../login.css";
 const Login = () => {
-    return <p>Helo</p>
-}
+  //   const fetchUser = () => {
+  //     const respons = await
+  //   };
+  //   useEffect(() => {
+  //     fetchUser();
+  //   }, []);
 
-export default Login
+  return (
+    <div className="container" id="login-container">
+      <div className="card">
+        <form action="">
+          <div className="username">
+            <label htmlFor="username">Username or Email</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="button-div">
+            <button className="login-button">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
